14e4fe74bc2bfb2c90e494a1f1df8c41680407ef
Andrew Heiss <andrewheiss@gmail.com>
2013-02-27 15:14:56 -0500

    Update code for rough draft writeup
    Includes cleaner LaTeX output and new ologit model and simulated
    predicted probabilities

--- initial_replications.R
+++ initial_replications.R
@@ -3,6 +3,18 @@
 # www.journalofdemocracy.org/articles/gratis/TesslerGraphics-19-1.pdf
 ###################################
 
+# Set up
+library(car)
+library(ROCR)
+library(Hmisc)
+library(ggplot2)
+library(xtable)
+library(MIfuns)
+source('separationplot.R')
+
+set.seed(1234)
+
+
 #-------------------------------------------------
 # Table 1
 # Personal Religiosity and Support for Democracy
@@ -13,8 +25,10 @@ round(prop.table(all.countries.table, 2), 2)
 individual.countries.table <- xtabs(~ dem.best + quran + country.name, data=barometer)
 round(prop.table(individual.countries.table, c(3,2)), 2)
 
-# Alternative
-ftable(round(prop.table(xtabs(~ country.name + dem.best + quran, data=barometer), c(3,1)), 2))
+# Alternative method for making table
+results <- ftable(round(prop.table(xtabs(~ country.name + dem.best + quran, data=barometer), c(3,1)), 2))
+
+print(xtable(ftable2data.frame(results), caption="Frequency table created from ICPSR data", label="tab:table2", digits=3), size="\\small", include.rownames=FALSE)
 
 
 #-----------------------------------------------------------
@@ -22,18 +36,92 @@ ftable(round(prop.table(xtabs(~ country.name + dem.best + quran, data=barometer)
 # Binary Logistic Regression Models Estimating Support for
 # Secularism among Respondents Who Support Democracy
 #-----------------------------------------------------------
-# Data = limited to dem.best?
 
 # Dependent variable
 # 0 = Favors Islamic democracy; 1 = Favors secular democracy
 # Footnote 13 explains why q4013 is the best measure
-model <- glm(sec.dem ~ I(-quran) + I(-prime.minister) + 
-               I(-citizen.influence) + I(-maintain.order) + 
-               education + age + family.econ, 
-             data=barometer, subset=(dem.best==1 & country==1), 
-             family=binomial(link="logit")) 
-summary(model)
 
+all.countries <- glm(sec.dem ~ I(-quran) + I(-prime.minister) + 
+                       I(-citizen.influence) + I(-maintain.order) + 
+                       education + age + family.econ, 
+                     data=barometer, subset=(dem.best==1), 
+                     family=binomial(link="logit")) 
+summary(all.countries)
+
+separation.plot(all.countries$fitted.values, all.countries$y, line=TRUE)
+
+# ROC Curve
+auc <- somers2(all.countries$fitted.values, all.countries$y)[1]
+ROCRpred <- prediction(all.countries$fitted.values, all.countries$y)
+
+plot(performance(ROCRpred,'tpr','fpr'), main="ROC plot", lwd=3, colorize=TRUE)
+abline(0, 1, col="grey", lty=3, lwd=3)
+text(1, .1, paste("AUC =", round(auc, 3)), pos=2)
+
+# Individual countries
+run.country.models <- function(x) {
+  model <- glm(sec.dem ~ I(-quran) + I(-prime.minister) + 
+                 I(-citizen.influence) + I(-maintain.order) + 
+                 education + age + family.econ, 
+               data=barometer, subset=(dem.best==1 & country.name==x), 
+               family=binomial(link="logit")) 
+}
+
+country.models <- sapply(levels(barometer$country.name), FUN=run.country.models)
+
+# View everything
+all.models <- cbind("All Countries"=coefficients(all.countries), as.data.frame(country.models[1,]))
+xtable(all.models)
+
+
+# Simulate  ologit results for Jordan
+model.ologit <- polr(sec.dem.ordinal ~ I(-quran) + I(-prime.minister) + 
+                       I(-citizen.influence) + I(-maintain.order) + 
+                       education + age + family.econ, data=barometer, 
+                     subset=(dem.best==1 & country.name=="Jordan"), 
+                     method="logistic")
+
+
+# Predictions
+# Look at effect of education
+quran.range <- min(barometer$quran, na.rm=T):max(barometer$quran, na.rm=T)
+
+# Extract stuff from model
+beta.ologit <- coef(model.ologit)
+tau.ologit <- model.ologit$zeta
+
+my.mode <- function(x){
+  which.max(table(x))
+}
+
+X <- cbind(quran.range, my.mode(barometer$prime.minister), my.mode(barometer$citizen.influence), my.mode(barometer$maintain.order), my.mode(barometer$education), my.mode(barometer$age), my.mode(barometer$family.econ))
+
+# Simulation
+tau.beta <- function(x) {
+  beta <- x[1:7]
+  tau <- x[8:10]
+  p1 <- plogis(tau[1] - X %*% beta)
+  p2 <- plogis(tau[2] - X %*% beta) - plogis(tau[1] - X %*% beta)
+  p3 <- plogis(tau[3] - X %*% beta) - plogis(tau[2] - X %*% beta)
+  p4 <- 1 - plogis(tau[3] - X %*% beta)
+  return(data.frame(p1, p2, p3, p4))
+}
+
+runs <- 1000
+draw <- mvrnorm(runs, c(beta.ologit, tau.ologit), vcov(model.ologit))
+beta.sim <- draw[,1:7]
+tau.sim <- draw[,8:10]
+
+simulation.list <- apply(draw, 1, FUN=tau.beta)
+plot.data <- cbind(simulation=rep(1:runs, each=length(quran.range)), quran=rep(quran.range, runs), ldply(simulation.list))
+plot.data <- melt(plot.data, id.vars=c("simulation", "quran"), variable_name="category")
+plot.data$category <- factor(plot.data$category, labels=c("Strongly agree", "Agree", "Disagree", "Strongly dissagree"))
+
+ologit.spaghetti <- ggplot()
+ologit.spaghetti + geom_line(aes(x=quran, y=value, group=interaction(simulation, category), colour=category), alpha=0.05, data=plot.data, size=1) + 
+  scale_colour_brewer(palette="Set1") +
+  guides(colour = guide_legend(override.aes = list(alpha = 1, size=2))) + 
+  labs(y="Probability of answering\n", x="\nFrequency of Qur'an reading", colour="Secular democracy")
 
 
 #---------------------------------------------------
@@ -43,4 +131,4 @@ summary(model)
 # Respondents Who Support Democracy
 #---------------------------------------------------
 
-
+# Do this once Table 2 is accurate
--- rebuild_data.R
+++ rebuild_data.R
@@ -1,17 +1,19 @@
-# Create a more useable subset of the original tab-delimited Arab Barometer file
+## @knitr rebuild_data
+#-------------------------
+# Load initial libraries
+#-------------------------
 library(car)
+
 
 #------------------------
 # Import and clean data
 #------------------------
+setwd("~/Documents/Duke 2012-2013/Spring 2013/Polsci 733/Attitudes in the Arab World Replication")
 barometer.raw <- read.table("ICPSR_26581/DS0001/26581-0001-Data.tsv", sep="\t", header=TRUE)
 
 # 97, 98, and 99 excluded
 #is.na(barometer.raw) <- barometer.raw >= 97 & barometer.raw <= 99
-barometer.clean <- recode(barometer.raw, '97:99=NA')
-
-# Set working dataset---temporary thing?
-barometer <- barometer.clean
+barometer <- recode(barometer.raw, '97:99=NA')
 
 
 #----------------
@@ -19,6 +21,7 @@ barometer <- barometer.clean
 #----------------
 barometer$dem.best <- as.factor(recode(barometer$q2324, "1:2=1; 3:4=0"))
 barometer$sec.dem <- recode(barometer$q4013, "1:2=0; 3:4=1")
+barometer$sec.dem.ordinal <- factor(barometer$q4013, labels=c("Strongly agree", "Agree", "Disagree", "Strongly dissagree"))
 barometer$country.name <- (factor(barometer$country, levels=c(1, 2, 3, 4, 6, 7), labels=c("Jordan", "Palestine", "Algeria", "Morocco", "Lebanon", "Yemen")))
 
 # Rename variables for sake of simplicity
@@ -29,7 +32,3 @@ barometer$maintain.order <- barometer$q2323
 barometer$education <- barometer$q703
 barometer$age <- barometer$q701agecategories
 barometer$family.econ <- barometer$q103
-
-
-# Convenience data frame
-barometer.dem.best <- barometer[barometer$dem.best == 1,]
